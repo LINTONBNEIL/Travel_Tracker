04c554364d9b3be53c86fd330c7d36aa54a390ee
Neil Linton <lintonbneil@gmail.com>
2022-06-11 10:04:31 -0700

    Add function for past trips display dom

--- src/scripts.js
+++ src/scripts.js
@@ -12,7 +12,13 @@ import TripsRepo from '../src/TripsRepo.js'
 import Trip from '../src/Trip.js'
 
 //----QUERY SELECTORS----
-let travelerName = document.querySelector('.traveler-greeting')
+let travelerName = document.querySelector('.traveler-greeting');
+// let tripCard = document.querySelector('.traveler-card');
+let presentBox = document.querySelector('.Present');
+let upcomingBox = document.querySelector('.Upcoming');
+let pendingBox = document.querySelector('.Pending');
+let pastBox = document.querySelector('.Past');
+
 
 
 //----GLOBAL VARIABLES----
@@ -26,35 +32,90 @@ let tripsArray;
 let tripsRepo;
 let destinationsArray;
 let destinationRepo;
+let travelersTrips
+
+let date = '2022/5/11';
 
 //----EVENT LISTENERS----
-window.addEventListener('load', loadData);
-
-//----DOM FUNCTIONS----
+window.addEventListener('load', () => {
+  allData.then(data => {
+  travelerData = data[0].travelers
+  destinationData = data[1].destinations
+  tripsData = data[2].trips
+  initialSetup(travelerData, destinationData, tripsData)
+  }).catch(error => console.log(error))
+});
 
 //----DATA FUNCTIONS----
-function loadData() {
-  allData.then(data => {
-    travelerData = data[0],
-    destinationData = data[1],
-    tripsData = data[2]
-    initialSetup(true)
-  }).catch(error => console.log(error))
+
+const initialSetup = (travelers, destinations, trips) => {
+  createTraveler(travelers)
+  createDestinations(destinations)
+  createTrips(trips)
 };
 
-  function initialSetup(first) {
-    travelerArray = travelerData.travelerData.map(traveler => new Traveler(traveler));
-    travelerRepo = new TravelerRepository(travelerArray);
-    tripsArray = tripsData.tripsData.map(trip => new Trip(trip));
-    tripsRepo = new Trips(tripsArray)
-    destinationsArray = destinationData.destinationData.map(destination => new Destination(destination));
-    destinationRepo = new DestinationRepo(destinationsArray)
-    if (first) {
-      randomTraveler = getRandomUser(travelerRepo.travelerData)
-    }
-  };
+const createTraveler = (travelerData) => {
+travelerArray = travelerData.map(traveler => new Traveler(traveler));
+travelerRepo = new TravelerRepository(travelerArray)
+randomTraveler = getRandomUser(travelerRepo.travelerData)
+displayTravelerInfo(randomTraveler)
+};
 
-  function getRandomUser(array) {
+const createDestinations = (destinationData) => {
+  destinationsArray = destinationData.map(destination => new Destination(destination));
+  destinationRepo = new DestinationRepo(destinationsArray)
+};
+
+const createTrips = (tripData) => {
+  tripsArray = tripsData.map(trip => new Trip(trip));
+  tripsRepo = new TripsRepo(tripsArray)
+  findTravelerTrips(tripsRepo)
+  setTripsDestination()
+};
+
+const findTravelerTrips = (tripsRepo) => {
+  travelersTrips = tripsRepo.findAllTravelerTrip(randomTraveler.id)
+  findPresentTrips(travelersTrips)
+}
+
+const setTripsDestination = () => {
+  travelersTrips.forEach(trip => {
+  let destinationObj = destinationRepo.findDestination(trip.destinationID);
+  trip.destinationID = destinationObj;
+});
+}
+
+const findPresentTrips = (travelersTrips) => {
+return travelersTrips.find(trip => {
+  console.log("past", trip)
+    if (trip.date > date) {
+      console.log("trip", trip)
+    }
+    displayPastTrips(travelersTrips)
+  })
+}
+
+const getRandomUser = (array) => {
     let randomIndex = Math.floor(Math.random() * array.length)
     return array[randomIndex]
-  };
+};
+
+//----DOM FUNCTIONS----
+const displayTravelerInfo = (randomTraveler) => {
+  travelerName.innerHTML = `Welcome, ${randomTraveler.name}`
+}
+
+const displayPastTrips = (travelersTrips) => {
+  travelersTrips.forEach(trip => {
+    pastBox.innerHTML += `
+    <div class="traveler-card">
+      <image class="picture">Destination Picture</image>
+      <p>Destination Name</p>
+      <p>Destination Cost lodging/flight</p>
+      <p>${trip.date}</p>
+      <p>${trip.duration}</p>
+      <p>${trip.travelers}</p>
+    </div>
+    `
+  })
+}
